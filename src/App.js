39f6ed409257655959538f198466568255c1047d
John Goehner <john.goehner@gmx.de>
2021-08-09 11:46:45 +0200

    Import Switch, Route and Link from react-router-dom and add routes to header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import "./App.css";
+import { Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header>
-        <p>Home</p>
-        <p>Menu</p>
+        <Switch>
+          <Route path="/analyse">
+            <p>Analyse</p>
+            <Link to="/">
+              <p>Menu</p>
+            </Link>
+          </Route>
+          <Route path="/collection">
+            <p>Collection</p>
+            <Link to="/">
+              <p>Menu</p>
+            </Link>
+          </Route>
+          <Route path="/playlists">
+            <p>Playlists</p>
+            <Link to="/">
+              <p>Menu</p>
+            </Link>
+          </Route>
+          <Route path="/project-info">
+            <p>Project Info</p>
+            <Link to="/">
+              <p>Menu</p>
+            </Link>
+          </Route>
+          <Route path="/">
+            <p>Home</p>
+            <Link to="/">
+              <p>Menu</p>
+            </Link>
+          </Route>
+        </Switch>
       </header>
       <main>
         <nav>
