810823ec7cf44b2ea4be84034b851942046ac5c9
John Goehner <john.goehner@gmx.de>
2021-08-09 11:39:10 +0200

    Write markdown for / page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <header>
+        <p>Home</p>
+        <p>Menu</p>
+      </header>
+      <main>
+        <nav>
+          <ul>
+            <li>Analyse</li>
+            <li>Collection</li>
+            <li>Playlists</li>
+            <li>Project Info</li>
+          </ul>
+        </nav>
+      </main>
+    </div>
+  );
 }
 
 export default App;
