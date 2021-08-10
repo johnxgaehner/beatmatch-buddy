c848637eeebb50ccae045858f6ffb133b9b05f73
John Goehner <john.goehner@gmx.de>
2021-08-10 18:47:10 +0200

    Insert basic calculation to average the tap tempo

--- src/pages/AnalysePage.js
+++ src/pages/AnalysePage.js
@@ -1,9 +1,39 @@
+import { useState } from "react";
 import "./AnalysePage.css";
 
 export default function AnalysePage() {
+  const [BPM, setBpm] = useState("tap to start");
+
+  let lastTapSeconds = 0;
+  let beatCounter = [];
+  let average = 0;
+  let count = 0;
+
+  function tapTempo() {
+    const tapSeconds = Date.now();
+    const bpm = (1 / ((tapSeconds - lastTapSeconds) / 1000)) * 60;
+
+    lastTapSeconds = tapSeconds;
+    beatCounter.push(Math.floor(bpm));
+
+    average *= count;
+    average += Math.floor(bpm);
+    count++;
+    average /= count;
+
+    if (beatCounter.length >= 4) {
+      setBpm(average);
+    }
+  }
+
+  function renderBPM() {
+    return <p>{BPM}</p>;
+  }
+
   return (
-    <section className="AnalysePage">
+    <section onClick={tapTempo} className="AnalysePage">
       <div className="BPM">TAP</div>
+      {renderBPM()}
     </section>
   );
 }
