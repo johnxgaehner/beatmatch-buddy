1d4f73d44fab607516290c74bbfd0f9a1882b884
John Goehner <john.goehner@gmx.de>
2021-08-11 16:56:13 +0200

    Move text-transform: uppercase into .App.css

--- src/pages/CollectionPage.js
+++ src/pages/CollectionPage.js
@@ -3,17 +3,16 @@ import CollectionItem from "../components/CollectionItem";
 import "./CollectionPage.css";
 
 export default function CollectionPage() {
-  const [items, setItems] = useState();
+  const [collection, setCollection] = useState();
 
   useEffect(() => {
-    const savedTracks = JSON.parse(localStorage.getItem("savedTracks"));
-    setItems(savedTracks);
-    console.log(savedTracks);
+    const storedTracks = JSON.parse(localStorage.getItem("savedTracks"));
+    setCollection(storedTracks);
   }, []);
 
   function renderCollectionItems() {
-    if (items) {
-      const collectionItems = items.map((track, index) => {
+    if (collection) {
+      const collectionItems = collection.map((track) => {
         return <CollectionItem key={track.id} data={track} />;
       });
       return collectionItems;
@@ -22,33 +21,41 @@ export default function CollectionPage() {
 
   return (
     <section className="CollectionPage">
-      <div className="CollectionPage__SearchFilter">
-        <input
-          type="text"
-          name="SearchFilter"
-          id="SearchFilter"
-          placeholder="SEARCH:"
-        />
-      </div>
-      <div className="CollectionPage__TempoFilter">
-        <p>TEMPO:</p>
-        <div className="CollectionPage__TempoFilterSection">
-          <input
-            type="text"
-            name="TempoFilter__min"
-            id="TempoFilter__min"
-            placeholder="128"
-          />
-          <p>-</p>
-          <input
-            type="text"
-            name="TempoFilter__max"
-            id="TempoFilter__max"
-            placeholder="132"
-          />
+      {collection ? (
+        <>
+          <div className="CollectionPage__SearchFilter">
+            <input
+              type="text"
+              name="SearchFilter"
+              id="SearchFilter"
+              placeholder="SEARCH:"
+            />
+          </div>
+          <div className="CollectionPage__TempoFilter">
+            <p>TEMPO:</p>
+            <div className="CollectionPage__TempoFilterSection">
+              <input
+                type="text"
+                name="TempoFilter__min"
+                id="TempoFilter__min"
+                placeholder="128"
+              />
+              <p>-</p>
+              <input
+                type="text"
+                name="TempoFilter__max"
+                id="TempoFilter__max"
+                placeholder="132"
+              />
+            </div>
+          </div>
+          {renderCollectionItems()}
+        </>
+      ) : (
+        <div className="CollectionPage__Empty-Collection">
+          YOUR COLLECTION IS EMPTY
         </div>
-      </div>
-      {renderCollectionItems()}
+      )}
     </section>
   );
 }
