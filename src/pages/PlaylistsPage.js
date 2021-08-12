e693e0c437d402ca0203ef5dfcfb45f9f564f0b7
John Goehner <john.goehner@gmx.de>
2021-08-12 15:29:47 +0200

    Add markup + basic styling

--- src/pages/PlaylistsPage.js
+++ src/pages/PlaylistsPage.js
@@ -1,5 +1,16 @@
 import "./PlaylistsPage.css";
+import { ReactComponent as IconArrow } from "../assets/icon_arrow.svg";
 
 export default function PlaylistsPage() {
-  return "hi";
+  return (
+    <section className="PlaylistsPage">
+      <div className="PlaylistsPage__CreateNewPlaylist">
+        <p>CREATE NEW PLAYLIST</p>
+      </div>
+      <div className="PlaylistsPage__PlaylistItem">
+        <p>18/09 MOIRÉ</p>
+        <IconArrow />
+      </div>
+    </section>
+  );
 }
