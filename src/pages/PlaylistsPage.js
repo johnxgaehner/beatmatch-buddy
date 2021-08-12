6491f0fcdebf786914a51902c622dfeb6b15ac22
John Goehner <john.goehner@gmx.de>
2021-08-12 15:43:31 +0200

    Finish markup and set up Links

--- src/pages/PlaylistsPage.js
+++ src/pages/PlaylistsPage.js
@@ -1,16 +1,17 @@
 import "./PlaylistsPage.css";
 import { ReactComponent as IconArrow } from "../assets/icon_arrow.svg";
+import { Link } from "react-router-dom";
 
 export default function PlaylistsPage() {
   return (
     <section className="PlaylistsPage">
-      <div className="PlaylistsPage__CreateNewPlaylist">
+      <Link className="PlaylistsPage__CreateNewPlaylist">
         <p>CREATE NEW PLAYLIST</p>
-      </div>
-      <div className="PlaylistsPage__PlaylistItem">
+      </Link>
+      <Link className="PlaylistsPage__PlaylistItem">
         <p>18/09 MOIRÉ</p>
         <IconArrow />
-      </div>
+      </Link>
     </section>
   );
 }
