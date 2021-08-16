ca3b307146f361975f90b80ece2623a4c4d0c91d
John Goehner <john.goehner@gmx.de>
2021-08-16 15:24:48 +0200

    Get saved tracks & playlists from LocalStorage on first render

--- src/pages/PlaylistDetailPage.js
+++ src/pages/PlaylistDetailPage.js
@@ -4,5 +4,15 @@ import { useParams } from "react-router-dom";
 export default function PlaylistDetailPage() {
   const { playlistId } = useParams();
 
+  const [tracks, setTracks] = useState();
+  const [playlists, setPlaylists] = useState();
+
+  useEffect(() => {
+    const savedTracks = JSON.parse(localStorage.getItem("savedTracks"));
+    const savedPlaylists = JSON.parse(localStorage.getItem("savedPlaylists"));
+    setTracks(savedTracks);
+    setPlaylists(savedPlaylists);
+  }, []);
+
   return <p>{playlistId}</p>;
 }
