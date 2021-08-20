9aa5008384d3ed1d952f50928fe7e11d0fb13ab8
John Goehner <john.goehner@gmx.de>
2021-08-20 12:03:56 +0200

    Add emoji in project info

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,23 +4,25 @@ import { ReactComponent as IconArrow } from "../assets/icon_arrow.svg";
 
 export default function HomePage() {
   return (
-    <nav className="navigation">
-      <NavLink className="Row--flat --space-between" to="/analyse">
-        <p>Analyse</p>
-        <IconArrow />
-      </NavLink>
-      <NavLink className="Row--flat --space-between" to="/collection">
-        <p>Collection</p>
-        <IconArrow />
-      </NavLink>
-      <NavLink className="Row--flat --space-between" to="/playlists">
-        <p>Playlists</p>
-        <IconArrow />
-      </NavLink>
-      <NavLink className="Row--flat --space-between" to="/project-info">
-        <p>Project Info</p>
-        <IconArrow />
-      </NavLink>
-    </nav>
+    <section className="HomePage">
+      <nav className="navigation">
+        <NavLink className="Row--flat --space-between" to="/analyse">
+          <p>Analyse</p>
+          <IconArrow />
+        </NavLink>
+        <NavLink className="Row--flat --space-between" to="/collection">
+          <p>Collection</p>
+          <IconArrow />
+        </NavLink>
+        <NavLink className="Row--flat --space-between" to="/playlists">
+          <p>Playlists</p>
+          <IconArrow />
+        </NavLink>
+        <NavLink className="Row--flat --space-between" to="/project-info">
+          <p>Project Info</p>
+          <IconArrow />
+        </NavLink>
+      </nav>
+    </section>
   );
 }
