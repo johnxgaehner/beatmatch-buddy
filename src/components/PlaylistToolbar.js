fb0a0ccbcad1fb0423023ce4605f01dae68d5bd0
John Goehner <john.goehner@gmx.de>
2021-08-27 12:10:49 +0200

    Clean PlaylistToolbar

--- src/components/PlaylistToolbar.js
+++ src/components/PlaylistToolbar.js
@@ -1,3 +1,4 @@
+import { render } from "@testing-library/react";
 import PropTypes from "prop-types";
 import "./PlaylistToolbar.css";
 
@@ -19,25 +20,38 @@ export default function PlaylistToolbar({
     onDeletePlaylistClick();
   }
 
+  function renderToolbar() {
+    if (editMode) {
+      return (
+        <>
+          <button
+            onClick={handleDeletePlaylistClick}
+            className="PlaylistToolbar__DeleteButton"
+          >
+            Delete Playlist
+          </button>
+          <button onClick={toggleEditMode}>Save</button>
+        </>
+      );
+    }
+    if (addTracksMode) {
+      return (
+        <>
+          <button onClick={toggleAddTracksMode}>Save</button>
+        </>
+      );
+    }
+    return (
+      <>
+        <button onClick={toggleAddTracksMode}>Add Tracks</button>
+        <button onClick={toggleEditMode}>Edit Playlist</button>
+      </>
+    );
+  }
+
   return (
     <section className="Row--flat --accented --space-between">
-      {!editMode ? (
-        <button onClick={toggleAddTracksMode}>
-          {!addTracksMode ? "Add Tracks" : "Save"}
-        </button>
-      ) : (
-        <button
-          onClick={handleDeletePlaylistClick}
-          className="PlaylistToolbar__DeleteButton"
-        >
-          Delete Playlist
-        </button>
-      )}
-      {!addTracksMode && (
-        <button onClick={toggleEditMode}>
-          {!editMode ? "Edit Playlist" : "Save"}
-        </button>
-      )}
+      {renderToolbar()}
     </section>
   );
 }
