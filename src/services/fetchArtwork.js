15c8017a2c4e88940146258f129fbb80e6c3284c
John Goehner <john.goehner@gmx.de>
2021-08-31 15:29:07 +0200

    Extract function to get ArtworkUrl a bit more

--- src/services/fetchArtwork.js
+++ src/services/fetchArtwork.js
@@ -1,20 +1,9 @@
 export default async function fetchArtwork(newTrack) {
   const authEndpoint = "https://accounts.spotify.com/api/token";
-  const authString =
-    "55b9f829a2e7449da28119bc679d6b70:5d3899109bb040f5b708ff630c8630be";
+  const clientId = "55b9f829a2e7449da28119bc679d6b70";
+  const clientSecret = "5d3899109bb040f5b708ff630c8630be";
+  const authString = `${clientId}:${clientSecret}`;
   const authorization = Buffer.from(authString).toString("base64");
-
-  const authToken = await fetch(authEndpoint, {
-    method: "post",
-    body: "grant_type=client_credentials",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      Authorization: `Basic ${authorization}`,
-    },
-  })
-    .then((res) => res.json())
-    .then((data) => data.access_token)
-    .catch((error) => console.dir(error));
 
   const apiEndpoint = "https://api.spotify.com/v1/search";
   const artistSearchParam = newTrack.artistName
@@ -25,7 +14,28 @@ export default async function fetchArtwork(newTrack) {
     .replace(/[^a-zA-Z0-9+äÄöÖüÜß]/g, "");
   const searchUrl = `${apiEndpoint}?q=${artistSearchParam}+${trackSearchParam}&type=album,track&limit=1`;
 
-  const artworkUrl = await fetch(searchUrl, {
+  const authToken = await fetchToken(authEndpoint, authorization);
+  const artworkUrl = await fetchArtworkUrl(searchUrl, authToken);
+
+  return artworkUrl;
+}
+
+async function fetchToken(authEndpoint, authorization) {
+  return await fetch(authEndpoint, {
+    method: "post",
+    body: "grant_type=client_credentials",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${authorization}`,
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => data.access_token)
+    .catch((error) => console.dir(error));
+}
+
+async function fetchArtworkUrl(searchUrl, authToken) {
+  return await fetch(searchUrl, {
     method: "get",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
@@ -39,6 +49,4 @@ export default async function fetchArtwork(newTrack) {
         : data.albums.items[0].images[2].url
     )
     .catch((error) => console.dir(error));
-
-  return artworkUrl;
 }
